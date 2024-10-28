5308d443c64e66ae8e6e1d0c40dac654b11387d3
Zolan <zolannp2602@gmail.com>
2024-10-28 18:59:53 +0100

    json-server pas finis

--- src/app/components/root/root.component.ts
+++ src/app/components/root/root.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterModule, CommonModule, MatButtonModule, MatMenuModule ],
+  imports: [RouterModule, CommonModule, MatButtonModule, MatMenuModule],
   templateUrl: './root.component.html',
   styleUrl: './root.component.css'
 })
 export class RootComponent {
-
 }

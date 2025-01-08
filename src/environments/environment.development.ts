e641952f4e045ccc54befd6485c2010fddb83369
zolangivre <zolannp2602@gmail.com>
2025-01-08 11:13:45 +0100

    url changes heroku deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://festivawin-back-16b79a35ef75.herokuapp.com',
+  apiUrl: 'https://festivawin-back-16b79a35ef75.herokuapp.com/api',
 };

baf60610510216d4bf898a2db9bfc9ebe0b9e5d9
zolangivre <zolannp2602@gmail.com>
2025-01-18 15:29:26 +0100

    ajout background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SessionService } from './services/session.service';
-import { Router, RouterModule } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { CountdownComponent } from './components/countdown/countdown.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { Renderer2 } from '@angular/core';
 
 @Component({
-    selector: 'app-app',
-    imports: [RouterModule, CountdownComponent, MatButtonModule, MatIconModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-app',
+  imports: [
+    RouterModule,
+    CommonModule,
+    CountdownComponent,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public isActive: boolean = false;
-
-  constructor(private sessionService: SessionService, private router: Router) {}
+  public isHomePage: boolean = false;
+  constructor(
+    private sessionService: SessionService,
+    private router: Router,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     this.sessionService.isSessionActive().subscribe(
@@ -39,6 +54,18 @@ export class AppComponent {
     setInterval(() => {
       this.sessionService.updateSessionStatus().subscribe();
     }, 1000);
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        if (this.router.url === '/accueil') {
+          this.renderer.addClass(document.body, 'home-background');
+          this.renderer.removeClass(document.body, 'other-background');
+        } else {
+          this.renderer.addClass(document.body, 'other-background');
+          this.renderer.removeClass(document.body, 'home-background');
+        }
+      });
   }
   redirectToLogin(): void {
     this.router.navigate(['/login']);
--- src/app/components/bilan/bilan-acheteur/bilan-acheteur.component.ts
+++ src/app/components/bilan/bilan-acheteur/bilan-acheteur.component.ts
@@ -7,22 +7,26 @@ import { Vente } from '../../../models/vente';
 
 @Component({
   selector: 'app-bilan-acheteur',
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './bilan-acheteur.component.html',
   styleUrl: './bilan-acheteur.component.css',
 })
 export class BilanAcheteurComponent implements OnChanges {
   @Input() utilisateur!: Utilisateur;
   sommesDepenses: number = 0;
-  quantiteJeuAchete: number = 0;
+  nbJeuAchete: number = 0;
   nbAchat: number = 0;
 
   constructor(
     private venteService: VenteService,
-    private venteJeuService: VenteJeuService,
-  ) { }
+    private venteJeuService: VenteJeuService
+  ) {}
+
+  ngOnInit(): void {
+    this.sommesDepenses = 0;
+    this.nbJeuAchete = 0;
+    this.nbAchat = 0;
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['utilisateur'] && this.utilisateur) {
@@ -31,14 +35,21 @@ export class BilanAcheteurComponent implements OnChanges {
   }
 
   updateBilan(): void {
-    this.venteService.getVentesByAcheteurId(this.utilisateur._id).subscribe((achats) => {
-      this.nbAchat = achats.length;
-      achats.forEach((achat : Vente) => {
-        this.sommesDepenses += achat.montantTotal;
-        this.venteJeuService.getJeuxByVenteId(achat._id).subscribe((venteJeux) => {
-          this.quantiteJeuAchete += venteJeux.length;
+    this.sommesDepenses = 0;
+    this.nbJeuAchete = 0;
+    this.nbAchat = 0;
+    this.venteService
+      .getVentesByAcheteurId(this.utilisateur._id)
+      .subscribe((achats) => {
+        this.nbAchat = achats.length;
+        achats.forEach((achat: Vente) => {
+          this.sommesDepenses += achat.montantTotal;
+          this.venteJeuService
+            .getJeuxByVenteId(achat._id)
+            .subscribe((venteJeux) => {
+              this.nbJeuAchete = venteJeux.length;
+            });
         });
       });
-    });
   }
 }
